f900ed9ee180b2052ea4c480e782f5461ed8d2b8
hekdk <desaidev034@gmail.com>
2025-05-29 16:17:08 +0530

    Created a CounterApp

---
+++ Java/CounterApp/app/src/main/java/com/example/counterapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.counterapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView welcome_text, counter_text;
+    Button btn;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        welcome_text = findViewById(R.id.welcome_text);
+        counter_text = findViewById(R.id.counter_text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter_text.setText("" + increaseCounter());
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public  int increaseCounter(){
+        return ++counter;
+    }
+}
