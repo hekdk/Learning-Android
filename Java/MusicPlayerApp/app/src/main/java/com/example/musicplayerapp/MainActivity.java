8ced31ccd86d4327d2eca84211fa81e206f812ba
hekdk <desaidev034@gmail.com>
2025-06-19 12:59:30 +0530

    created a simple service app learned about creating service and implementing them

--- Java/MusicPlayerApp/app/src/main/java/com/example/musicplayerapp/MainActivity.java
+++ Java/MusicPlayerApp/app/src/main/java/com/example/musicplayerapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.musicplayerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
+    Button start_btn, stop_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.textView);
+        start_btn = findViewById(R.id.start_btn);
+        stop_btn = findViewById(R.id.stop_btn);
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent serviceIntent = new Intent(getApplicationContext(), CustomService.class);
+                startService(serviceIntent);
+            }
+        });
+
+        stop_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(getApplicationContext(), CustomService.class);
+                stopService(serviceIntent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
